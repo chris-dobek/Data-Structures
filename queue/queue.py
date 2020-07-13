907db599adff2cef8c8c017569b7c699d042c281
Chris Dobek <dobek.christopher@gmail.com>
2020-07-13 16:31:25 -0400

    Day 1 MVP

--- queue/queue.py
+++ queue/queue.py
@@ -18,7 +18,7 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList
+        self.storage = LinkedList()
     
     def __len__(self):
         return self.size
