0c365f26e8928af0fb02049faf7419c14e7ff46b
Chris Dobek <dobek.christopher@gmail.com>
2020-07-14 17:01:08 -0400

    singly_linked_list.py passes test

--- stack/singly_linked_list.py
+++ stack/singly_linked_list.py
@@ -71,3 +71,19 @@ class LinkedList:
                 return True
             current = current.get_next()
         return False
+    
+    def get_max(self):
+        if not self.head:
+            return None
+        
+        max_value = self.head.get_value()
+        current = self.head.get_value()
+
+        while current:
+            if current.get_value() > max_value:
+                max_value = current.get_value()
+            
+            current = current.get_next()
+        
+        return max_value
+    
